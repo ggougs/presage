8d4c67532c9a19d7ade0267a8489a8feead2fe8a
benjamin vanhaudenarde <benjamin.vanhaudenarde@gmail.com>
2018-09-18 15:47:06 +0200

    versionben

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
 console.log("toto"); 
 
+    //Fonction appelée lorsque l'on clique sur le lien Afficher la fenêtre
+    $('.read-more').on('click', function(){
+            if($(this).hasClass('selected')){
+        deselect($(this)); 
+        }else{
+                $(this).addClass('selected');  
+            }
+    return false;
+    });
+    //Fonction appelée lorsque l'on clique sur le lien Fermer la fenêtre
+    $('.close').on('click', function(){
+    deselect($('#afficherPopup'));
+    return false;
+    });
 
+   function deselect(e) {
+    e.removeClass('selected');
+   }
 }); 
--- src/Controller/ActualiteController.php
+++ src/Controller/ActualiteController.php
@@ -19,16 +19,26 @@ class ActualiteController extends AbstractController
     // }
 
     /**
-     * @Route("/accueil", name="accueil")
+     * @Route("/actualite", name="actualite")
      */
     public function afficherActualité(ActualiteRepository $repository){
 
         $actualiteArray = $repository->findAll();
-        return $this->render('accueil/index.html.twig', [
+        return $this->render('actualite/index.html.twig', [
             'controller_name' => 'ActualiteController',
             'actualite' => $actualiteArray,
         ]);
 
+    }
+     /**
+     * @Route("/accueil", name="accueil")
+     */
+    public function acutaliteMiseEnAvant(ActualiteRepository $repository){
+        $actuAvantArray = $repository->findOneById(1);
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'ActualiteController',
+            'actuAvant' => $actuAvantArray,
+        ]);
     }
 
 }
