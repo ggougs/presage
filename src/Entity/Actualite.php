eafc11bdc89d0ec7aaaa46bfa0424c7cd61729df
Garabedian <garabedian.g@gmail.comgit config --global user.email garabedian.g@gmail.com>
2018-09-26 15:14:27 +0200

    greg

--- src/Entity/Actualite.php
+++ src/Entity/Actualite.php
@@ -28,10 +28,14 @@ class Actualite
     private $contenu;
 
     /**
-     * @Assert\File(mimeTypes={ "image/png" ,"image/jpg","image/jpeg" })
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $image;
+
+    /**
+     * @Assert\File(mimeTypes={ "image/png" ,"image/jpg","image/jpeg" })
+     */
+    private $imageUpload;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -80,6 +84,18 @@ class Actualite
     public function setImage(?string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    public function getImageUpload(): ?string
+    {
+        return $this->imageUpload;
+    }
+
+    public function setImageUpload(?string $imageUpload): self
+    {
+        $this->imageUpload = $imageUpload;
 
         return $this;
     }
