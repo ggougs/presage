c8b025097dbc0dabe1aa55f2dafe7dbba132d8ca
Mathieu Delclos <mathieu.delclos@laposte.net>
2018-09-26 15:04:44 +0200

    mathieu toffre des cigarettes

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Actualite;
 
 class SearchController extends AbstractController
 {
@@ -15,24 +16,28 @@ class SearchController extends AbstractController
      * @Route("/search", name="search")
      */
     public function search(Request $request) {
-        if ($request->getMethod() == "POST") {
-            $search = $request->request->get('search');
-       
+        $results = array();
+        if ($request->getMethod() == "GET") {
+            $search = $request->query->get('search');
+            dump($search);
             $datas = explode(" ", $search);
        
             $em = $this->getDoctrine()->getManager();
             $query = $em->createQueryBuilder();
             $query
-                ->select('titre', 'contenu')
-                ->from('actualite', 'a');
+                ->select('a.titre, a.contenu')
+                ->from(Actualite::class, 'a');
             $i = 0;
+            dump($datas);
             foreach ($datas as $data) {
                 $query
                     ->andWhere('a.titre LIKE :data'.$i.' OR a.contenu LIKE :data'.$i)
                     ->setParameter('data'.$i, '%'.$data.'%');
                $i++;
             }
-            $results = $query->getQuery()->getResult(); 
+            $results = $query->getQuery()->getResult();
+            dump($query->getQuery());
+            dump($results);
         }
         return $this->render('search/search.html.twig', array(
             'results' => $results
